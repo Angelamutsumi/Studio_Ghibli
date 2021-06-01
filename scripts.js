b5c4edd9466de8d3667e6c84d871752f4e900f5e
Angelamutsumi <angelamutsumi@gmail.com>
2021-05-31 22:05:47 -0300

    Adiciona  logo e container no scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement(img);
+logo.src = 'logo.png';
+
+const container =  document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+
 fetch('https://ghibliapi.herokuapp.com/films')
   .then((response) => {
     return response.json();
+    var data = JSON.parse(this.response);
   })
   .then((data) => {
-    console.log(data);
+    data.forEach((movie) => {
+      console.log(movie.title);
+    })
   })
   .catch((err) => {
     console.log('Erro no consumo da API');
