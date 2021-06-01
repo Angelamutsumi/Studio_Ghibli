e4c1962f37b1912acdbb2c63893be7aa916ffd67
Angelamutsumi <angelamutsumi@gmail.com>
2021-05-31 22:11:25 -0300

    Adiciona cards

--- scripts.js
+++ scripts.js
@@ -17,7 +17,20 @@ fetch('https://ghibliapi.herokuapp.com/films')
   })
   .then((data) => {
     data.forEach((movie) => {
-      console.log(movie.title);
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}`;
+
+      container.appendChild(card);
+
+      card.appendChild(h1);
+      card.appendChild(p);
     })
   })
   .catch((err) => {
